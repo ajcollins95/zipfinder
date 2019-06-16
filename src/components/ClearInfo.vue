eeed5123e060f61a4c48606f9d3af0e2448b6c88
Aaron Collins <aa.collins.95@gmail.com>
2019-06-16 00:29:51 -0400

    Before website becomes progressive

---
+++ src/components/ClearInfo.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <ion-button
+  color='light'
+  expand='block'
+  v-if="info"
+  @click="$emit('clear-info')">
+    Clear
+  </ion-button>
+
+</template>
+
+<script>
+export default {
+  name: "ClearInfo",
+  props: ['info']
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Zip Info</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ZipSearch @get-zip="getZipInfo"/>
+      <ZipInfo :info="info"/>
+      <ClearInfo :info="info" @clear-info="clearInfo"/>
+
+    </ion-content>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ZipSearch from "../components/ZipSearch"
+import ZipInfo from "../components/ZipInfo"
+import ClearInfo from "../components/ClearInfo"
+
 
 export default {
   name: 'home',
+  data() {
+    return {
+      info: null
+    }
+  },
   components: {
-    HelloWorld
+    ZipSearch,
+    ZipInfo,
+    ClearInfo
+
+  },
+  methods: {
+    async getZipInfo(zip) {
+      const res = await fetch(`https://api.zippopotam.us/us/${zip}`)
+      if (res.status == 404) {
+        this.showAlert()
+      }
+      const info = await res.json()
+      this.info = info
+      //console.log(info)
+    },
+    clearInfo() {
+      this.info = null
+    },
+    showAlert() {
+      return this.$ionic.alertController
+        .create({
+          header: "Not Valid",
+          message: "Please enter a valid US zipcode",
+          buttons: ["Ok"]
+        })
+        .then(a => a.present())
+    }
   }
 }
 </script>
