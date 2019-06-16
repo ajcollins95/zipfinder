f6265b6e264ad7b657007d9294a781f3cab1fc6e
Aaron Collins <aa.collins.95@gmail.com>
2019-06-16 00:51:07 -0400

    pwa added... (in the future, start with pwa option/plugin)

--- src/components/ClearInfo.vue
+++ src/components/ClearInfo.vue
@@ -1,17 +1,14 @@
-<template lang="html">
-  <ion-button
-  color='light'
-  expand='block'
-  v-if="info"
-  @click="$emit('clear-info')">
+<template>
+  <ion-button color='light' expand='block' v-if="info" @click="$emit('clear-info')">
     Clear
   </ion-button>
-
 </template>
 
 <script>
+
 export default {
   name: "ClearInfo",
   props: ['info']
 }
+
 </script>
